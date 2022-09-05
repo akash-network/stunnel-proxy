43f23d042c03a0cda1f14af8ddc6c07fe328149f
Tom Beynon <tom@tombeynon.co.uk>
2022-09-05 12:25:53 +0100

    Convert to confd

--- Dockerfile
+++ Dockerfile
@@ -2,8 +2,12 @@ FROM debian:buster
 
 RUN apt-get update && apt-get -y --no-install-recommends install stunnel4
 
+COPY confd /etc/confd
 COPY configure.sh /usr/local/configure.sh
 RUN chmod +x /usr/local/configure.sh
+
+ADD https://github.com/kelseyhightower/confd/releases/download/v0.16.0/confd-0.16.0-linux-amd64 /usr/local/bin/confd
+RUN chmod +x /usr/local/bin/confd
 
 ENTRYPOINT [ "/usr/local/configure.sh" ]
 
--- configure.sh
+++ configure.sh
@@ -9,34 +9,6 @@ EOF
 chown stunnel4 /etc/stunnel/node.psk
 chmod 0600 /etc/stunnel/node.psk
 
-cat > /etc/stunnel/stunnel.conf << EOF
-# stunnel client config for TMKMS server
-
-setuid = stunnel4
-setgid = nogroup
-
-pid = /var/run/stunnel4/stunnel4.pid
-
-foreground = yes
-
-# for debugging
-debug = 7
-
-[rpc]
-client  = $CLIENT
-accept  = $RPC_ACCEPT
-connect = $RPC_HOST
-ciphers = PSK
-PSKidentity = node
-PSKsecrets = /etc/stunnel/node.psk
-
-[signer]
-client  = $CLIENT
-accept  = $SIGNER_ACCEPT
-connect = $SIGNER_HOST
-ciphers = PSK
-PSKidentity = node
-PSKsecrets = /etc/stunnel/node.psk
-EOF
+confd -onetime -backend env
 
 exec "$@"
